7eeea501b8ea0127c9cb54299352fbc1f5439134
AServantDev <arnaud.github@gmail.com>
2021-01-26 06:54:33 +0100

    deleting useless state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@
 import React, {Component} from 'react'
 import  Header  from "./component/interface/Header";
 import List from './component/contact/List'
-import Content from './component/Content'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Provider} from './context'
 import AddContact from './component/contact/AddContact'
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
 import Error from './component/interface/Error'
 class App extends Component {
-  state = {
-    nom1:"Arnaud",
-    nom2: "Ayano",
-    nom3: "Léo"
-  }
   render(){
     return (
       
